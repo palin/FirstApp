0642d2969cd3eb468409a3046e4e0dc6143aea02
Maciek Paliński <maciekpalinski@gmail.com>
2015-02-01 14:00:48 +0000

    squash emove spaces from different places
    remove more spaces
    
    correct indentation
    
    use present? method
    
    remove comment
    
    remove duplication

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -2,7 +2,6 @@ class CommentsController < ApplicationController
   before_filter :require_user
 
   def create
-    # params[:comment] => { :receiver_id => 2, :body => 'asdasd' }
     @comment = current_user.comments.build(params[:comment])
 
     if @comment.save
@@ -13,7 +12,7 @@ class CommentsController < ApplicationController
       render :template => 'users/show'
     end
   end
-  
+
   def delete
   	@comment_to_delete = current_user.comments.first(:conditions => ["id= ?", params[:id]]).destroy
   	redirect_to(user_path(current_user.id), :notice => "Usunieto komentarz")

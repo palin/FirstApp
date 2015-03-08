1715f19d78cdf5caefc32fa0ccde31345283756e
Maciej Palinski <maciekpalinski@gmail.com>
2015-03-08 21:14:14 +0000

    extract private method

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -2,7 +2,7 @@ class CommentsController < ApplicationController
   before_filter :require_user
 
   def create
-    @comment = current_user.comments.build(params[:comment])
+    @comment = current_user.comments.build(comment_params)
 
     if @comment.save
       redirect_to(user_path(@comment.receiver), :notice => "Utworzono komentarz")
@@ -16,6 +16,12 @@ class CommentsController < ApplicationController
   def delete
   	@comment_to_delete = current_user.comments.first(:conditions => ["id= ?", params[:id]]).destroy
   	redirect_to(user_path(current_user.id), :notice => "Usunieto komentarz")
+  end
+
+  private
+
+  def comment_params
+    params[:comment]
   end
 
 end
